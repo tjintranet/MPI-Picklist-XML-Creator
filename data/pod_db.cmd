f954d4caf57b934d3b9bb511e18302cf382e6c3c
tjintranet <112164769+tjintranet@users.noreply.github.com>
2025-10-10 09:24:41 +0100

    Delete data directory

--- data/pod_db.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-"C:\Program Files\EasyDataTransform_1_42_0\EasyDataTransform_1_42_0.exe" "\\192.168.10.169\pipeline3\clays_POD\data\Create Data JSON.transform" -file "PRH Master Titles=\\192.168.10.169\pipeline3\clays_POD\data\PRH Master Titles.xlsx[Sheet1]" -file "data1=\\192.168.10.169\pipeline3\clays_POD\data\data.json" -cli -verbose
-
-rem
-exit
-
